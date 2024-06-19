9688bff7997d84e009b1031ebddf8c1addc1a8e8
James Park-Watt <james@park-watt.co.uk>
2024-06-19 08:56:53 +0100

    fix: corrected project config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Takserver - Unofficial Documentation'
+copyright = 'None, See LICENSE'
+author = 'James Park-Watt'
 
 release = '0.1'
 version = '0.1.0'
